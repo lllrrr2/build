e090a3f2780aa03b4be61aacb83e37ef7e00d745
hong0980 <hong0980@qq.com>
2022-06-06 16:32:57 +0800

    update@2022-06-06-16-32

--- luci-app-rebootschedule/luasrc/model/cbi/rebootschedule/general.lua
+++ luci-app-rebootschedule/luasrc/model/cbi/rebootschedule/general.lua
@@ -1,19 +1,15 @@
-fs = require "nixio.fs"
 sys = require "luci.sys"
-uci = require"luci.model.uci".cursor()
 util = require "luci.util"
-font_color = [[<br><font color=orange>➤</font>]]
+uci = require "luci.model.uci".cursor()
 m = Map("rebootschedule", translate("定时任务 Plus+"), 
 translate("<font color='green'><b>让定时任务更加易用的插件，是用 wulishui@gmail.com 的原版修改。</font></b><br>") ..
 translate("CRON表达式的字符串是五个有空格分隔加执行命令字段组成：<font color=red> * * * * * [command]</font><br>") ..
-translate("五个星号按照位置分别表示(分)，(时)，(日)，(月)，(周)，(commond)表示所需执行的命令。<br>") ..
-translate("允许的数值范围是：分(0-59)，时(0-23)，日(1-31)，月(1-12)，周(1-7)。<br><br>") ..
-translate("<font color=orange>▼</font>每一个字段都可以使用范围之内的数值或者以下半角的特殊字符组合组成：") .. font_color ..
-translate("1) 星号(*)表示<code>任意值</code>。在(分)字段使用(*), 表示每分钟执行命令。") .. font_color ..
-translate("2) 正斜杠(/)表示<code>每隔时段</code>。在(分)字段使用(*/20)，表示每小时中每隔20分钟执行命令。") .. font_color ..
-translate("3) 逗号(,)表示<code>分割数值</code>。在(分)字段使用(5,20)，表示每小时中每到5分和20分时执行命令。") .. font_color ..
-translate("4) 连字符(-)表示<code>范围</code>。在(分)字段使用(5-20)，表示从每小时中5分到20分时段每分钟执行命令。<br>") ..
-translate("<font color='green'><b>设置方法可点击下面查看示例或在  crontab文件  中行尾是　#rebootschedule　的命令前5个字段去验证是否正确</b></font><br>") ..
+translate("按照位置的定义以及取值范围分别表示：分(0-59)，时(0-23)，日(1-31)，月(1-12)，周(1-7)，(commond)表示所需执行的命令。<br>") ..
+translate("[ , ]逗号表示<strong>列出枚举值</strong>。在分钟域中，5,20表示分别在第5分钟和20分钟触发一次。<br>") ..
+translate("[ / ]正斜杠表示<strong>指定数值的增量</strong>。在分钟域中，3/20表示从每20分钟的第3分钟触发一次。<br>") ..
+translate("[ - ]连字符表示<strong>范围</strong>。在分钟域中，5-20表示从5分钟到20分钟之间每隔一分钟触发一次。<br>") ..
+translate("[ * ]星号表示<strong>所有可能的值</strong>。在月域中，*表示每个月；在星期域中，*表示星期的每一天。<br>") ..
+translate("<font color='green'><b>设置方法也可点击下面查看示例或在  crontab文件  中行尾是　#rebootschedule　的命令前5个字段去验证是否正确</b></font><br>") ..
 translate("<input class='cbi-button cbi-button-apply' type='button' value='查看示例'  onclick=\"window.open('http://'+window.location.hostname+'/reboothelp.jpg')\"/>&nbsp;&nbsp;&nbsp;&nbsp;") ..
 translate("<input class='cbi-button cbi-button-apply' type='button' value='查看/验证' onclick=\"window.open('https://tool.lu/crontab')\"/>"))
 
