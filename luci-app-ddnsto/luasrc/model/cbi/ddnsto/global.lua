137b6d16e451962244b61c385c66d94bfe105deb
hong0980 <hong0980@qq.com>
2022-06-06 17:23:39 +0800

    update@2022-06-06-17-23

--- luci-app-ddnsto/luasrc/model/cbi/ddnsto/global.lua
+++ luci-app-ddnsto/luasrc/model/cbi/ddnsto/global.lua
@@ -1,8 +1,6 @@
 m = Map("ddnsto", translate("DDNSTO 内网穿透"))
-m.description = translate("DDNSTO是koolshare小宝开发支持HTTP2.0快速远程穿透的工具。") .. 
-translate("<br><br><input class=\"cbi-button cbi-button-apply\" type=\"button\" value=\"" .. 
-translate("注册与教程") ..
-"\" onclick=\"window.open('https://www.ddnsto.com')\"/>")
+m.description = translate("简单、快速的内网穿透工具，不受网络限制，全局掌控您的私人设备") .. 
+translate("&nbsp;&nbsp;&nbsp;&nbsp;<input class='cbi-button cbi-button-apply' type='button' value='注册与教程' onclick='window.open(\"https://www.ddnsto.com\")'/>")
 m:section(SimpleSection).template = "ddnsto/ddnsto_status"
 
 t = m:section(TypedSection, "global")
@@ -17,8 +15,11 @@ e.rmempty = false
 -- e.password = true
 e.datatype = "rangelength(36,36)"
 
-e = t:option(Value, "start_delay", translate("延迟启动"), translate("单位：秒"))
+e = t:option(Value, "start_delay", translate("延迟启动"))
 e.datatype = "uinteger"
+for _, v in ipairs({10, 20, 30, 40, 50, 60}) do
+	e:value(v, translate("%u 秒") %{v})
+end
 e.default = "20"
 e.rmempty = true
 
