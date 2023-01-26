e50586e871aa8dd8dc2139acc6422a64f0b4bc83
hong0980 <hong0980@qq.com>
2023-01-26 19:34:15 +0800

    update@2023-01-26-19-34

--- luci-app-pwdHackDeny/luasrc/model/cbi/pwdHackDeny.lua
+++ luci-app-pwdHackDeny/luasrc/model/cbi/pwdHackDeny.lua
@@ -2,28 +2,27 @@ local fs  = require "nixio.fs"
 local sys = require "luci.sys"
 local m,s
 -- local button = ""
-local running=(luci.sys.call("pidof pwdHackDeny.sh > /dev/null") == 0)
-local a1=(luci.sys.call("[ `grep -c 'Bad password attempt' /etc/pwdHackDeny/badip.log.ssh 2>/dev/null` -gt 0 ]") == 0)
-local b1=(luci.sys.call("[ `grep -c 'failed login on' /etc/pwdHackDeny/badip.log.web 2>/dev/null` -gt 0 ]") == 0)
 
-if running then
-	state_msg = "<b><font color=\"green\">" .. translate("正在运行") .. "</font></b>"
+if (luci.sys.call("pidof pwdHackDeny.sh > /dev/null") == 0) then
+	state_msg = [[<b><font color="green">]] .. translate("正在运行") .. [[</font></b>]]
 else
-	state_msg = "<b><font color=\"red\">" .. translate("没有运行") .. "</font></b>"
+	state_msg = [[<b><font color="red">]] .. translate("没有运行") .. [[</font></b>]]
 end
-if a1 then
-	state_msg1 = "<b><font color=\"red\">" .. translate("有SSH异常登录！") .. "</font></b>"
+
+if (luci.sys.call("[ `grep -c 'Bad password attempt' /etc/pwdHackDeny/badip.log.ssh 2>/dev/null` -gt 0 ]") == 0) then
+	state_msg1 = "有SSH异常登录！"
 else
-	state_msg1 = "<font color=\"gray\">SSH最近登录日志</font>"
+	state_msg1 = "SSH最近登录日志"
 end
-if b1 then
-	state_msg2 = "<b><font color=\"red\">" .. translate("有WEB异常登录！") .. "</font></b>"
+
+if (luci.sys.call("[ `grep -c 'failed login on' /etc/pwdHackDeny/badip.log.web 2>/dev/null` -gt 0 ]") == 0) then
+	state_msg2 = "有WEB异常登录！"
 else
-	state_msg2 = "<font color=\"gray\">WEB最近登录日志</font>"
+	state_msg2 = "WEB最近登录日志"
 end
 
 m = Map("pwdHackDeny", translate("登录管制"))
-m.description = translate("监控SSH及WEB异常登录，密码错误累计达到 5 次的内外网客户端都禁止连接SSH以及WEB登录端口，<br>直到手动删除相应的IP或MAC名单为止。也可以在名单中添加排除项目，被排除的客户端将不会被禁止。" .. "<br><br>" .. translate("运行状态 : ") .. state_msg .."<br />")
+m.description = translate("监控SSH及WEB异常登录，密码错误累计达到 5 次的内外网客户端都禁止连接SSH以及WEB登录端口，<br>直到手动删除相应的IP或MAC名单为止。也可以在名单中添加排除项目，被排除的客户端将不会被禁止。<br><br>" .. translate("运行状态 : ") .. state_msg .."<br/>")
 
 s = m:section(TypedSection, "pwdHackDeny")
 s.anonymous=true
@@ -48,24 +47,22 @@ setport.default=5
 setport.datatype="port"
 setport.rmempty=false
 
-local aa2=(luci.sys.call("[ `sed -n '$=' /etc/pwdHackDeny/badip.log.ssh 2>/dev/null` -gt 0 ]") == 0)
-if aa2 then
+if (luci.sys.call("[ `sed -n '$=' /etc/pwdHackDeny/badip.log.ssh 2>/dev/null` -gt 0 ]") == 0) then
 	clearsshlog = s:option(Button, "clearsshlog", translate("清除SSH登录日志"))
 	clearsshlog.inputtitle = translate("清除")
 	clearsshlog.inputstyle = "apply"
 	function clearsshlog.write(self, section)
-	   luci.sys.exec("cat /dev/null > /etc/pwdHackDeny/badip.log.ssh &")
+	   luci.sys.exec(":> /etc/pwdHackDeny/badip.log.ssh &")
 	   luci.http.redirect(luci.dispatcher.build_url("admin/control/pwdHackDeny"))
 	end
 end
 
-local bb2=(luci.sys.call("[ -s /etc/pwdHackDeny/badip.log.web ]") == 0)
-if bb2 then
+if (luci.sys.call("[ -s /etc/pwdHackDeny/badip.log.web ]") == 0) then
 	clearlwebog = s:option(Button, "clearlwebog", translate("清除WEB登录日志"))
 	clearlwebog.inputtitle = translate("清除")
 	clearlwebog.inputstyle = "apply"
 	function clearlwebog.write(self, section)
-	   luci.sys.exec("cat /dev/null > /etc/pwdHackDeny/badip.log.web &")
+	   luci.sys.exec(":> /etc/pwdHackDeny/badip.log.web &")
 	   luci.http.redirect(luci.dispatcher.build_url("admin/control/pwdHackDeny"))
 	end
 end
@@ -96,7 +93,7 @@ function conf.cfgvalue()
 end
 
 if (luci.sys.call("[ -s /etc/pwdHackDeny/badhosts.web ] ") == 0) then
-	s:tab("config3", translate("<font style='color:brown'>WEB累积记录</font>"))
+	s:tab("config3", translate("WEB累积记录"))
 	conf = s:taboption("config3", Value, "editconf3", nil, translate("<font style='color:red'>新的信息需要刷新页面才会显示。如记录中有自己的MAC，可复制到相应的黑名单中，在前面加#可避免被屏蔽。</font>"))
 	conf.template = "cbi/tvalue"
 	conf.rows = 20
@@ -109,7 +106,7 @@ if (luci.sys.call("[ -s /etc/pwdHackDeny/badhosts.web ] ") == 0) then
 end
 
 if (luci.sys.call("[ -s /etc/pwdHackDeny/badhosts.ssh ] ") == 0) then
-	s:tab("config4", translate("<font style='color:brown'>SSH累积记录</font>"))
+	s:tab("config4", translate("SSH累积记录"))
 	conf = s:taboption("config4", Value, "editconf4", nil, translate("<font style='color:red'>新的信息需要刷新页面才会显示。如记录中有自己的MAC，可复制到相应的黑名单中，在前面加#可避免被屏蔽。</font>"))
 	conf.template = "cbi/tvalue"
 	conf.rows = 20
@@ -121,7 +118,7 @@ if (luci.sys.call("[ -s /etc/pwdHackDeny/badhosts.ssh ] ") == 0) then
 		end
 end
 
-s:tab("config5", translate("<font style='color:black'>SSH禁止名单</font>"))
+s:tab("config5", translate("SSH禁止名单"))
 conf = s:taboption("config5", Value, "editconf5", nil, translate("<font style='color:red'>预设名单内外网都可以添加IP或MAC地址，IP4段格式为192.168.18.10-20，不能为192.168.1.1/24或192.168.18.10-192.168.18.20。<br>自动拦截的内网名单仅自动添加MAC地址。</font>"))
 conf.template = "cbi/tvalue"
 conf.rows = 20
@@ -140,7 +137,7 @@ function conf.write(self, section, value)
     end
 end
 
-s:tab("config6", translate("<font style='color:black'>WEB禁止名单</font>"))
+s:tab("config6", translate("WEB禁止名单"))
 conf = s:taboption("config6", Value, "editconf6", nil, translate("<font style='color:red'>预设名单内外网都可以添加IP或MAC地址，IP4段格式为192.168.18.10-20，不能为192.168.1.1/24或192.168.18.10-192.168.18.20。<br>自动拦截的内网名单仅自动添加MAC地址。</font>"))
 conf.template = "cbi/tvalue"
 conf.rows = 20
@@ -159,8 +156,7 @@ function conf.write(self, section, value)
     end
 end
 
-e = luci.http.formvalue("cbi.apply")
-if e then
+if (luci.http.formvalue("cbi.apply")) then
   io.popen("/etc/init.d/pwdHackDeny start")
 end
 
--- luci-app-wizard/luasrc/model/cbi/wizard.lua
+++ luci-app-wizard/luasrc/model/cbi/wizard.lua
@@ -5,7 +5,6 @@ s.addremove = false
 s.anonymous = true
 
 s:tab('wansetup', translate('Wan Settings'), translatef('Three different ways to access the Internet, please choose according to your own situation.'))
-
 o = s:taboption('wansetup', ListValue, 'wan_proto', translate('Protocol'))
 o:value('dhcp', translate('DHCP client'))
 o:value('pppoe', translate('PPPoE'))
@@ -17,7 +16,7 @@ o = s:taboption('wansetup', Value, 'wan_pppoe_pass', translate('PAP/CHAP passwor
 o:depends('wan_proto', 'pppoe')
 o.password = true
 
-dns = s:taboption('wansetup', Value, 'lan_dns', translate('Use custom DNS servers'), translate("可以设置主路由的IP，或可到<a href='https://dnsdaquan.com' target='_blank'> DNS大全 </a>获取更多"))
+dns = s:taboption('wansetup', DynamicList, 'lan_dns', translate('Use custom DNS servers'), translate("可以设置主路由的IP，或可到<a href='https://dnsdaquan.com' target='_blank'> DNS大全 </a>获取更多"))
 dns:value("223.5.5.5", translate("阿里DNS：223.5.5.5"))
 dns:value("223.6.6.6", translate("阿里DNS：223.6.6.6"))
 dns:value("101.226.4.6", translate("DNS派：101.226.4.6"))
@@ -28,19 +27,18 @@ dns:value("114.114.115.115", translate("114DNS：114.114.115.115"))
 dns.default = "223.5.5.5"
 dns.anonymous = false
 dns.datatype = "ip4addr"
-dns.cast = "string"
+-- dns.cast = "string"
 
 s:tab("lansetup", translate("Lan Settings"))
-
 ipaddr = s:taboption("lansetup", Value, "lan_ipaddr", translate("IPv4 address"), "LAN网口IP (<b><font color=\"red\">即是该路由web访问的IP</font></b>)")
-for own_ip in luci.util.execi("uci get network.lan.ipaddr") do
-	ipaddr:value(own_ip, translate(own_ip .. " --当前路由的IP--"))
-	ipaddr.default = own_ip
+for ip in luci.util.execi("uci -q get network.lan.ipaddr") do
+	ipaddr:value(ip, translate(ip .. " --当前路由的IP--"))
+	ipaddr.default = ip
 end
 ipaddr.datatype="ip4addr"
 ipaddr.anonymous = false
 
-netmask = s:taboption('lansetup', Value, 'lan_netmask', translate('IPv4 netmask'))
+netmask = s:taboption('lansetup', ListValue, 'lan_netmask', translate('IPv4 netmask'))
 netmask:value("255.255.255.0", translate("255.255.255.0"))
 netmask:value("255.255.0.0", translate("255.255.0.0"))
 netmask:value("255.0.0.0", translate("255.0.0.0"))
