39bf959438da8f92bfbe2edff240c93fd9d01ac2
hong0980 <hong0980@qq.com>
2023-02-09 15:06:50 +0800

    update@2023-02-09-15-06

--- luci-app-wizard/luasrc/model/cbi/wizard.lua
+++ luci-app-wizard/luasrc/model/cbi/wizard.lua
@@ -6,7 +6,7 @@ s = m:section(TypedSection, 'wizard')
 s.addremove = false
 s.anonymous = true
 
-s:tab('wansetup', translate('Wan Settings'), translatef('Three different ways to access the Internet, please choose according to your own situation.'))
+s:tab('wansetup', translate('Wan Settings'))
 wan_proto = s:taboption('wansetup', ListValue, 'wan_proto', translate('Protocol'))
 wan_proto:value('pppoe', translate('PPPoE'))
 wan_proto:value('dhcp', translate('DHCP client'))
@@ -37,7 +37,7 @@ wan_netmask:value('255.255.0.0')
 wan_netmask:value('255.0.0.0')
 wan_netmask:depends('wan_proto', 'static')
 wan_netmask.datatype = "ip4addr"
-wan_netmask.rmempty = false
+wan_netmask.rmempty = true
 wan_netmask.ucioption = 'netmask'
 
 local lan_ipaddr = (uci:get("network", "lan", "ipaddr") or "")
@@ -48,18 +48,13 @@ ipaddr.default = lan_ipaddr
 ipaddr.datatype="ip4addr"
 ipaddr.anonymous = false
 
-wan_dns  = s:taboption('wansetup', DynamicList, 'wan_dns', translate('WAN DNS 服务器'))
-wan_dns:depends('wan_proto', 'siderouter')
-wan_dns :value("223.5.5.5", translate("阿里DNS：223.5.5.5"))
-wan_dns :value("223.6.6.6", translate("阿里DNS：223.6.6.6"))
-wan_dns :value("101.226.4.6", translate("DNS派：101.226.4.6"))
-wan_dns :value("218.30.118.6", translate("DNS派：218.30.118.6"))
-wan_dns :value("180.76.76.76", translate("百度DNS：180.76.76.76"))
-wan_dns :value("114.114.114.114", translate("114DNS：114.114.114.114"))
-wan_dns :value("114.114.115.115", translate("114DNS：114.114.115.115"))
-wan_dns .default = "223.5.5.5"
-wan_dns .anonymous = false
-wan_dns .datatype = "ip4addr"
+netmask = s:taboption('wansetup', ListValue, 'lan_netmask', translate('IPv4 netmask'))
+netmask:value("255.255.255.0", translate("255.255.255.0"))
+netmask:value("255.255.0.0", translate("255.255.0.0"))
+netmask:value("255.0.0.0", translate("255.0.0.0"))
+netmask.default = "255.255.255.0"
+netmask.datatype='ip4addr'
+netmask.anonymous = false
 
 dns = s:taboption('wansetup', DynamicList, 'lan_dns', translate('LAN DNS 服务器'), translate("可以设置主路由的IP，或可到<a href='https://dnsdaquan.com' target='_blank'> DNS大全 </a>获取更多"))
 dns:value("223.5.5.5", translate("阿里DNS：223.5.5.5"))
@@ -74,36 +69,65 @@ dns.anonymous = false
 dns.datatype = "ip4addr"
 -- dns.cast = "string"
 
-netmask = s:taboption('wansetup', ListValue, 'lan_netmask', translate('IPv4 netmask'))
-netmask:value("255.255.255.0", translate("255.255.255.0"))
-netmask:value("255.255.0.0", translate("255.255.0.0"))
-netmask:value("255.0.0.0", translate("255.0.0.0"))
-netmask.default = "255.255.255.0"
-netmask.datatype='ip4addr'
-netmask.anonymous = false
+siderouter = s:taboption("wansetup", Flag, "enable_siderouter", translate("旁路由设置"))
+siderouter:depends('wan_proto', 'siderouter')
 
-lan_gateway = s:taboption('wansetup', Value, 'lan_gateway', translate('IPv4 gateway'), translate('这里输入主路由IP地址'))
-lan_gateway:depends('wan_proto', 'siderouter')
+lan_gateway = s:taboption('wansetup', Value, 'lan_gateway', translate('IPv4 gateway'), translate('这里输入主路由IP地址<b><font color="red"> 必须填写</font></b>'))
+lan_gateway:depends('enable_siderouter', true)
 lan_gateway.datatype = 'ip4addr'
-lan_gateway.rmempty = false
+lan_gateway.rmempty = true
 
 lan_sum = s:taboption("wansetup", Value, "lan_sum", translate("网口数量"),
 translate("该路由物理网口数量，留空则自动获取"))
-lan_sum:depends('wan_proto', 'siderouter')
+lan_sum.datatype = 'ufloat'
+lan_sum:depends('enable_siderouter', true)
 
 dhcp = s:taboption('wansetup', Flag, 'dhcp', translate('DHCP Server'), translate('开启此DHCP则需要关闭主路由的DHCP<br><b><font color="red">关闭主路由DHCP则需要手动将所有上网设备的网关和DNS改为此旁路由的IP</font></b>'))
 dhcp.datatype = 'ip4addr'
-dhcp:depends('wan_proto', 'siderouter')
--- dhcp.default = o.enabled
+dhcp:depends('enable_siderouter', true)
+-- dhcp.default = o.enable_siderouterd
 
 wan = s:taboption("wansetup", Flag, "wan_lan", translate("修改WAN口"),
 translate("修改WAN口变成LAN口"))
 wan.rmempty = true
-wan:depends('wan_proto', 'siderouter')
+wan:depends('enable_siderouter', true)
+
+wan_dns_1  = s:taboption('wansetup', DynamicList, 'wan_dns_1', translate('WAN DNS 服务器'))
+wan_dns_1:depends('wan_lan', true)
+wan_dns_1 :value("223.5.5.5", translate("阿里DNS：223.5.5.5"))
+wan_dns_1 :value("223.6.6.6", translate("阿里DNS：223.6.6.6"))
+wan_dns_1 :value("101.226.4.6", translate("DNS派：101.226.4.6"))
+wan_dns_1 :value("218.30.118.6", translate("DNS派：218.30.118.6"))
+wan_dns_1 :value("180.76.76.76", translate("百度DNS：180.76.76.76"))
+wan_dns_1 :value("114.114.114.114", translate("114DNS：114.114.114.114"))
+wan_dns_1 :value("114.114.115.115", translate("114DNS：114.114.115.115"))
+wan_dns_1 .default = "223.5.5.5"
+wan_dns_1 .anonymous = false
+wan_dns_1 .datatype = "ip4addr"
+
+wan_ipaddr_1 = s:taboption('wansetup', Value, 'wan_ipaddr_1', translate('IPv4 address'))
+wan_ipaddr_1:depends('wan_lan', true)
+wan_ipaddr_1.datatype = 'ip4addr'
+wan_ipaddr_1.rmempty = true
+wan_ipaddr_1.ucioption = 'ipaddr'
+
+wan_gateway_1 = s:taboption('wansetup', Value, 'wan_gateway_1', translate('IPv4 gateway'))
+wan_gateway_1:depends('wan_lan', true)
+wan_gateway_1.datatype = "ip4addr"
+wan_gateway_1.ucioption = 'gateway'
+
+wan_netmask_1 = s:taboption('wansetup', ListValue, 'wan_netmask_1', translate('IPv4 netmask'))
+wan_netmask_1:value('255.255.255.0')
+wan_netmask_1:value('255.255.0.0')
+wan_netmask_1:value('255.0.0.0')
+wan_netmask_1:depends('wan_lan', true)
+wan_netmask_1.datatype = "ip4addr"
+wan_netmask_1.rmempty = true
+wan_netmask_1.ucioption = 'netmask'
 
 firewall = s:taboption("wansetup", Flag, "firewall", translate("防火墙设置"))
 firewall.rmempty = true
-firewall:depends('wan_proto', 'siderouter')
+firewall:depends('enable_siderouter', true)
 
 fullcone = s:taboption("wansetup", Flag, "fullcone", translate("SYN-flood"),
 translate("关闭防火墙ISYN-flood防御服务<code>建议开启</code>"))
