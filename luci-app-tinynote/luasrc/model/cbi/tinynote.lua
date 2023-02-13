59272e929f2451cf64a5cf1f3f6134d27432b6f1
hong0980 <hong0980@qq.com>
2023-02-13 14:21:50 +0800

    update@2023-02-13-14-21

--- luci-app-tinynote/luasrc/model/cbi/tinynote.lua
+++ luci-app-tinynote/luasrc/model/cbi/tinynote.lua
@@ -1,10 +1,16 @@
 local fs = require "nixio.fs" --加载函数
 local sys = require "luci.sys" --加载函数
+local uci = require "luci.model.uci".cursor()
 --wulishui 20200108-20210804
 
-m = Map("tinynote", translate(""), translate([[<font color="green"><b>只能记录少量文本内容。文本内容勿大于90Kb（约1000行），否则无法保存。</b></font>]]))
+if (uci:get_bool("luci", "tinynote", "tinynote") == 1) then
+	uci:set("luci", "tinynote", "tinynote")
+	uci:commit("luci")
+end
+
+m = Map("luci", translate(""), translate([[<font color="green"><b>只能记录少量文本内容。文本内容勿大于90Kb（约1000行），否则无法保存。</b></font>]]))
 s = m:section(TypedSection, "tinynote")
-s.anonymous=true
+s.anonymous = true
 
 if (sys.call("test ! -d /etc/tinynote")) then --判断目录是否存在
 	fs.mkdir("/etc/tinynote") --新建文件夹
@@ -12,29 +18,29 @@ end
 
 array = {1,2,3,4,5,6,7,8} --数组
 for i,v in ipairs(array) do
-	local file= (v .. ".txt")
-	if not fs.access("/etc/tinynote/tinynote" .. file) then --判断文件是否存在
-		sys.exec(":>/etc/tinynote/tinynote" .. file) --新建文件
+	local file = ("/etc/tinynote/tinynote" .. v .. ".txt")
+	if not fs.access(file) then --判断文件是否存在
+		sys.exec(":>" .. file) --新建文件
 	end
 
-	if fs.access("/etc/tinynote/tinynote" .. file) then
-		local Note= ("Note" .. v)
+	if fs.access(file) then
+		local Note = ("Note" .. v)
 		s:tab(Note, translate("笔记" .. v))
 		Note = s:taboption(Note, Value, "editNote" .. v, nil)
 		Note.template = "cbi/tvalue"
 		Note.rows = 35
 		Note.wrap = "off"
 		function Note.cfgvalue(self, section)
-			return fs.readfile("/etc/tinynote/tinynote" .. file) or ""
+			return fs.readfile(file) or ""
 		end
 		function Note.write(self, section, value)
 			if value then
 				value = value:gsub("\r\n?", "\n")
-				fs.writefile("/tmp/tinynote" .. file, value)
-				if (sys.call("cmp -s /tmp/tinynote" .. file .. " /etc/tinynote/tinynote" .. file) == 1) then
-					fs.writefile("/etc/tinynote/tinynote" .. file, value)
+				fs.writefile("/tmp/tinynote" .. v .. ".txt", value)
+				if (sys.call("cmp -s /tmp/tinynote" .. v .. ".txt" .. " " .. file) == 1) then
+					fs.writefile(file, value)
 				end
-				fs.remove("/tmp/tinynote" .. file)
+				fs.remove("/tmp/tinynote" .. v .. ".txt")
 			end
 		end
 	end
