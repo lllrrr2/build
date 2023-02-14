bf8e411ad80b18e15ed47ad56b69590d44b508db
hong0980 <hong0980@qq.com>
2023-02-14 22:44:22 +0800

    update@2023-02-14-22-44

--- luci-app-timedtask/luasrc/controller/timedtask.lua
+++ luci-app-timedtask/luasrc/controller/timedtask.lua
@@ -1,15 +1,16 @@
-local x = luci.model.uci.cursor()
 module("luci.controller.timedtask", package.seeall)
+
 function index()
 	if not nixio.fs.access("/etc/config/timedtask") then return end
-	entry({"admin", "services", "timedtask"}, alias("admin", "services", "timedtask", "general"), _("定时任务 Plus+"), 10) 
-	entry({"admin", "services", "timedtask", "general"}, cbi("timedtask/general"), _("时间设置"), 20).leaf = true
-	entry({"admin", "services", "timedtask", "crontab"}, form("timedtask/crontab"), _("crontab文件"), 30).leaf = true
+	entry({"admin", "services", "timedtask"}, alias("admin", "services", "timedtask", "general"), _("timedtask Plus+"), 10) 
+	entry({"admin", "services", "timedtask", "general"}, cbi("timedtask/general"), _("Timing settings"), 20).leaf = true
+	entry({"admin", "services", "timedtask", "crontab"}, form("timedtask/crontab"), _("crontabs"), 30).leaf = true
 	entry({"admin", "services", "timedtask", "button"}, post("button")).leaf = true
 end
 
 function button(sections)
-	command = x:get("timedtask", sections, "command")
+	local uci = require "luci.model.uci".cursor()
+	command = uci:get("timedtask", sections, "command")
     local e = {}
     -- cmd = "echo $(" .. command .. ")"
 	local p = io.popen(command)
--- luci-app-timedtask/luasrc/model/cbi/timedtask/general.lua
+++ luci-app-timedtask/luasrc/model/cbi/timedtask/general.lua
@@ -1,20 +1,30 @@
 sys = require "luci.sys"
 util = require "luci.util"
 uci = require "luci.model.uci".cursor()
-m = Map("timedtask", translate("定时任务 Plus+"), 
-translate("<font color='green'><b>让定时任务更加易用的插件，是用 wulishui@gmail.com 的原版修改。</font></b><br>") ..
-translate("CRON表达式的字符串是五个有空格分隔加执行命令字段组成：<font color=red> * * * * * [command]</font><br>") ..
-translate("按照位置的定义以及取值范围分别表示：分(0-59)，时(0-23)，日(1-31)，月(1-12)，周(1-7)，(commond)表示所需执行的命令。<br>") ..
-translate("[ , ]逗号表示<strong>列出枚举值</strong>。在分钟域中，5,20表示分别在第5分钟和20分钟触发一次。<br>") ..
-translate("[ / ]正斜杠表示<strong>指定数值的增量</strong>。在分钟域中，3/20表示从每20分钟的第3分钟触发一次。<br>") ..
-translate("[ - ]连字符表示<strong>范围</strong>。在分钟域中，5-20表示从5分钟到20分钟之间每隔一分钟触发一次。<br>") ..
-translate("[ * ]星号表示<strong>所有可能的值</strong>。在月域中，*表示每个月；在星期域中，*表示星期的每一天。<br>") ..
-translate([[设置方法也可点击下面查看示例或在  crontab文件  中行尾是　#timedtask　的命令前5个字段去验证是否正确<br><br>]]) ..
-translate([[<input class='cbi-button cbi-button-apply' type='button' value='查看示例'  onclick="window.open('http://'+window.location.hostname+'/reboothelp.jpg')">&nbsp;&nbsp;&nbsp;&nbsp;]]) ..
-translate([[<input class='cbi-button cbi-button-apply' type='button' value='查看/验证' onclick="window.open('https://tool.lu/crontab')"><br><br>]]))
+m = Map("timedtask", translate("timedtask Plus+"), 
+translate("<font color='green'><b>The plug-in that makes scheduled tasks easier to use is modified with the original version by wulishhui@gmail.com. </font></b><br>") ..
+translate("The string of the CRON expression is composed of five fields separated by spaces and the execution command: <font color=red> * * * * * [command]</font><br>") ..
+translate("According to the definition of the position and the range of values: minute (0-59), hour (0-23), day (1-31), month (1-12), week (1-7), (commond) The command to execute. <br>") ..
+translate("[ , ] comma means <strong>list enumeration values</strong>. In the minute field, 5,20 means triggering once at the 5th minute and 20th minute respectively. <br>") ..
+translate("[ / ] A forward slash indicates an <strong>increment of the specified value</strong>. In the minute field, 3/20 means triggering from the 3rd minute of every 20 minutes. <br>") ..
+translate("[ - ] hyphen indicates a <strong>range</strong>. In the minute field, 5-20 means trigger every minute from 5 minutes to 20 minutes. <br>") ..
+translate("[ * ] An asterisk indicates <strong>all possible values</strong>. In the month field, * means every month; in the week field, * means every day of the week. <br>") ..
+translate([[The setting method can also be clicked below to view the example or the first 5 fields of the command whose line ends in the crontab file is #timedtask to verify whether it is correct<br><br>]]) ..
+translate([[<input class='cbi-button cbi-button-apply' type='button' value='View example'  onclick="window.open('http://'+window.location.hostname+'/reboothelp.jpg')">&nbsp;&nbsp;&nbsp;&nbsp;]]) ..
+translate([[<input class='cbi-button cbi-button-apply' type='button' value='view/verify' onclick="window.open('https://tool.lu/crontab')"><br><br>]]))
 
-if (uci:get("system", "@system[0]", "cronloglevel") ~= 9) then
-    uci:set("system", "@system[0]", "cronloglevel", "9")
+f = m:section(TypedSection, "cronloglevel")
+f.anonymous = true
+f = f:option(ListValue, "cronloglevel", translate("Cron Log Level"))
+f:value(5, translate("Debug"))
+f:value(8, translate("Normal"))
+f:value(9, translate("Warning"))
+f.default = "9"
+
+local cr_level = uci:get("system", "@system[0]", "cronloglevel")
+local ti_level = uci:get("timedtask", "@cronloglevel[0]", "cronloglevel")
+if (ti_level ~= cr_level ) then
+    uci:set("system", "@system[0]", "cronloglevel", ti_level)
     uci:commit("system")
 end
 
@@ -25,28 +35,28 @@ s.addremove = true -- 添加
 -- s.extedit  =  true -- 修改
 -- s.sortable  =  true -- 移动
 
-enable = s:option(Flag, "enable", translate("启用"))
+enable = s:option(Flag, "enable", translate("enable"))
 enable.rmempty = false
 enable.default = 0
 
-enable = s:option(Flag, "enable_lock", translate("锁定"))
+enable = s:option(Flag, "enable_lock", translate("locking"))
 enable.rmempty = false
 enable.default = 0
 
-minute = s:option(Value, "minute", translate("分"))
+minute = s:option(Value, "minute", translate("minute"))
 minute.default = '0'
 
-hour = s:option(Value, "hour", translate("时"))
+hour = s:option(Value, "hour", translate("Hour"))
 hour.default = '5'
 
-day = s:option(Value, "day", translate("日"))
+day = s:option(Value, "day", translate("day"))
 day.default = '*'
 
-month = s:option(Value, "month", translate("月"))
+month = s:option(Value, "month", translate("Month"))
 month.default = '*'
 
-week = s:option(Value, "week", translate("周"))
-week:value('*', translate("每天"))
+week = s:option(Value, "week", translate("weeks"))
+week:value('*', translate("Every day"))
 week:value(1, translate("Monday"))
 week:value(2, translate("Tuesday"))
 week:value(3, translate("Wednesday"))
@@ -56,23 +66,23 @@ week:value(6, translate("Saturday"))
 week:value(7, translate("Sunday"))
 week.default = '*'
 
-command = s:option(Value, "command", translate("执行的任务"))
-command:value('sleep 5 && touch /etc/banner && reboot', translate("重启系统"))
-command:value('service network restart &', translate("重启网络"))
-command:value('ifdown wan && ifup wan', translate("重启wan"))
-command:value('ifup wan', translate("重新拨号"))
-command:value('ifdown wan', translate("关闭联网"))
-command:value('ifup wan', translate("打开联网"))
-command:value('wifi down', translate("关闭WIFI"))
-command:value('wifi up', translate("打开WIFI"))
-command:value('sync && echo 3 > /proc/sys/vm/drop_caches', translate("释放内存"))
-command:value('poweroff', translate("关闭电源"))
-command:value('logread | egrep -v "miniupnpd|uhttpd" | tail -20 > /etc/syslog.log', translate("保存日志"))
+command = s:option(Value, "command", translate("Task"))
+command:value('sleep 5 && touch /etc/banner && reboot', translate("Reboots"))
+command:value('service network restart &', translate("Restart network"))
+command:value('ifdown wan && ifup wan', translate("Restart wan"))
+command:value('ifup wan', translate("Redial"))
+command:value('ifdown wan', translate("Turn off networking"))
+command:value('ifup wan', translate("Turn on networking"))
+command:value('wifi down', translate("Turn off WIFI"))
+command:value('wifi up', translate("Turn on WIFI"))
+command:value('sync && echo 3 > /proc/sys/vm/drop_caches', translate("Free up memory"))
+command:value('poweroff', translate("Turn off the power"))
+command:value('logread | egrep -v "miniupnpd|uhttpd" | tail -20 > /etc/syslog.log', translate("save log"))
 command.rmempty = false
 
 -- m.template = "timedtask/run"
--- btn = s:option(Button, "_baa", translate("立即执行"))
--- btn.inputtitle = translate("执行")
+-- btn = s:option(Button, "_baa", translate("Executed immediately"))
+-- btn.inputtitle = translate("Execute")
 -- btn.inputstyle = "apply"
 -- btn.disabled = false
 -- btn.template = "timedtask/button"
