3017f3980ec435419224f71e26ea978321d3d274
hong0980 <hong0980@qq.com>
2023-02-26 19:31:03 +0800

    update@2023-02-26-19-31

--- luci-app-tinynote/luasrc/model/cbi/tinynote.lua
+++ luci-app-tinynote/luasrc/model/cbi/tinynote.lua
@@ -3,78 +3,107 @@ local sys = require "luci.sys"
 local uci = require "luci.model.uci".cursor()
 -- wulishui 20200108-20210804
 
-if (uci:get("luci", "tinynote") ~= "tinynote") then
+local note_type_array = {
+    ["sh"]  = "#!/usr/bin/env sh",
+    ["lua"] = "#!/usr/bin/env lua",
+    ["py"]  = "#!/usr/bin/env python",
+}
+
+if not uci:get("luci", "tinynote") then
 	uci:set("luci", "tinynote", "tinynote")
 	uci:commit("luci")
 end
 
-if (sys.call("test ! -d /etc/tinynote") == 0) then
-	fs.mkdir("/etc/tinynote")
+local contains = function(list, value)
+    for k, v in pairs(list) do
+        if v == value then
+            return true
+        end
+        if k == value then
+            return v
+        end
+    end
+    return false
+end
+
+local delenote = function(list1, list2)
+	for _, x in pairs(list1) do
+		if not contains(list2, x) then
+			fs.remove(x)
+		end
+	end
+end
+
+local new_note = function(file, note_type)
+    if contains(note_type_array, note_type) then
+        sys.exec('echo "' .. contains(note_type_array, note_type) ..'"  > ' .. file)
+    else
+        sys.exec(":> " .. file)
+    end
 end
 
 m = Map("luci", translate(""), translate([[<font color="green"><b>只能记录少量文本内容。文本内容勿大于90Kb（约1000行），否则无法保存。</b></font>]]))
 
 f = m:section(TypedSection, "tinynote")
-f.template = "cbi/tblsection"
+-- f.template = "cbi/tblsection"
 f.anonymous = true -- 删除
-f.addremove = true -- 添加
-f.extedit   = true -- 修改
-f.sortable  = true -- 移动
+-- f.addremove = true -- 添加
+-- f.extedit   = true -- 修改
+-- f.sortable  = true -- 移动
+
+note_path = f:option(Value, "note_path", translate("保存路径"))
+note_path.default = "/etc/tinynote"
+
+note_sum = f:option(Value, "note_sum", translate("文本数量"))
+note_sum.default = "8"
+note_sum.datatype = "ufloat"
 
 note_type = f:option(Value, "note_type", translate("文本类形"))
 note_type.default = "txt"
-note_type.datatype = ""
 note_type:value('txt', translate('txt'))
 note_type:value('sh', translate('sh'))
 note_type:value('js', translate('js'))
 note_type:value('py', translate('py'))
 note_type:value('lua', translate('lua'))
 
-note_sum = f:option(Value, "note_sum", translate("文本数量"))
-note_sum.default = "8"
-note_sum.datatype = "ufloat"
-
 s = m:section(TypedSection, "tinynote")
 s.anonymous = true
 s.addremove = false
 
-local note_type_array = {
-	"#!/usr/bin/env sh",
-	"#!/usr/bin/env lua",
-	"#!/usr/bin/env python"
-}
+local note_sum  = uci:get("luci", "tinynote", "note_sum")  or "1"
+local note_type = uci:get("luci", "tinynote", "note_type") or "txt"
+local note_path = uci:get("luci", "tinynote", "note_path") or "/etc/tinynote"
+if sys.call("test ! -d /etc/tinynote") == 0 then fs.mkdirr(note_path) end
+local arg1,arg2 = {},{}
 
-local note_sum = uci:get("luci", "tinynote", "note_sum")
-if not note_sum then note_sum = 1 end
-local xx = sys.exec("seq 1 " .. note_sum)
-for sum in string.gmatch(xx, "%w") do
-	local file = ("/etc/tinynote/tinynote" .. sum .. ".txt")
-	if not fs.access(file) then
-		sys.exec(":> " .. file)
-	end
+for sum = 1, note_sum do
+	local file = note_path .. "/tinynote" .. sum .. "." .. note_type
+	arg2[sum] = file
+	if sys.call("[ -s " .. file .. " ]") == 1 then new_note(file, note_type) end
 
-	if fs.access(file) then
-		local Note = ("Note" .. sum)
-		s:tab(Note, translate("笔记" .. sum))
-		if (sys.call("test -s " .. file) == 0) then
-			o = s:taboption(Note, Button, "editNote" .. sum)
-			o.inputtitle = translate("清空笔记 " .. sum)
-			o.inputstyle = "reset"
-			function o.write()
-				if sys.call(":>" .. file) == 0 then
-					luci.http.redirect(luci.dispatcher.build_url("admin/nas/tinynote"))
-				end
-			end
-		end
+	if sys.call("[ -s " .. file .. " ]") then
+		local note = ("note" .. sum)
+		s:tab(note, translate("笔记 " .. sum))
 
-		Note = s:taboption(Note, Value, "editNote" .. sum, nil)
-		Note.template = "cbi/tvalue"
-		Note.rows = 35
-		Note.wrap = "off"
-		function Note.cfgvalue(self, section)
+		-- if sys.call("[ $(sed -n '$=' " .. file .. ") -gt 1 ]") == 0 then
+		-- 	o = s:taboption(note, Button, "note" .. sum)
+		-- 	o.inputtitle = translate("清空笔记 " .. sum)
+		-- 	o.inputstyle = "reset"
+		-- 	function o.write()
+		-- 		new_note(file, note_type)
+		-- 		if uci:changes("luci") then uci:commit("luci") end
+		-- 		luci.http.redirect(luci.dispatcher.build_url("admin/nas/tinynote"))
+		-- 	end
+		-- end
+
+		note = s:taboption(note, Value, "note" .. sum .. "." .. note_type)
+		note.template = "cbi/tvalue"
+		note.rows = 35
+		note.wrap = "off"
+		function note.cfgvalue(self, section)
 			return fs.readfile(file) or ""
 		end
-		function Note.write(self, section, value)
+		function note.write(self, section, value)
 			if value then
 				value = value:gsub("\r\n?", "\n")
 				local old_value = fs.readfile(value)
@@ -85,5 +114,11 @@ for sum in string.gmatch(xx, "%w") do
 		end
 	end
 end
+
+for i in fs.dir(note_path) do arg1[i] = note_path .. "/" .. i end
+if not rawequal(arg1,arg2) then delenote(arg1,arg2) end
+arg1,arg2 = nil,nil
+
+m:append(Template("tinynote/config_editor"))
 
 return m
