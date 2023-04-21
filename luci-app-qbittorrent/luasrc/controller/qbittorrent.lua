6a34c6e63c16f3af331b7d538c4402ab82560a97
hong0980 <hong0980@qq.com>
2023-04-22 02:53:19 +0800

    update@2023-04-22-02-53

--- luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
+++ luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
@@ -1,4 +1,5 @@
 local sys  = require "luci.sys"
+local http = require "luci.http"
 local uci  = require "luci.model.uci".cursor()
 module("luci.controller.qbittorrent",package.seeall)
 
@@ -12,33 +13,38 @@ function index()
 	entry({"admin", "nas", "qbittorrent", "action_log"}, call("action_log_read")).leaf=true
 end
 
+local con = uci:get_all("qbittorrent", "main")
 function act_status()
-	local BinaryLocation = uci:get("qbittorrent", "main", "BinaryLocation") or "/usr/bin/qbittorrent-nox"
+	local BinaryLocation = con.BinaryLocation or "/usr/bin/qbittorrent-nox"
 	local status = {
 		pid = 0,
 		pat = "",
+		port = "",
+		https = "",
 		running = "";
 	}
-	status.pid = sys.exec("pidof " .. BinaryLocation) or 0
+	status.port  = con.port  or '8080'
+	status.https = con.https or 'false'
+	status.pid   = sys.exec("pidof " .. BinaryLocation) or 0
 	if BinaryLocation ~= "/usr/bin/qbittorrent-nox" then
 		status.pat = BinaryLocation
 	end
 	status.running = sys.call("ps | grep " .. BinaryLocation .. " | grep -v grep >/dev/null") == 0
-	luci.http.prepare_content("application/json")
-	luci.http.write_json(status)
+	http.prepare_content("application/json")
+	http.write_json(status)
 end
 
 function action_log_read()
 	local file = {
-		log = "",
+		log    = "",
 		syslog = "";
 	}
-	local log_dir = uci:get("qbittorrent", "main", "RootProfilePath") or "/tmp"
+	local log_dir  = con.RootProfilePath or "/tmp"
 	local log_file = log_dir .. "/qBittorrent/data/logs/qbittorrent.log"
 	if nixio.fs.access(log_file) then
-		file.log = sys.exec("tail -n 50 %s | sed 'x;1!H;$!d;x'" %log_file)
+		file.log = sys.exec("tail -n 30 %s | sed 'x;1!H;$!d;x'" %log_file)
 	end
-	file.syslog = sys.exec("/sbin/logread -e qbittorrent | tail -n 50")
-	luci.http.prepare_content("application/json")
-	luci.http.write_json(file)
+	file.syslog = sys.exec("/sbin/logread -e qbittorrent | tail -n 30")
+	http.prepare_content("application/json")
+	http.write_json(file)
 end
