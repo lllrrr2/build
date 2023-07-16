9f34418fd3aac6b06f8c2d432d32f1285432e0bd
hong0980 <hong0980@qq.com>
2023-07-16 21:33:15 +0800

    update@2023-07-16-21-33

--- luci-app-qbittorrent/luasrc/model/cbi/qbittorrent/files.lua
+++ luci-app-qbittorrent/luasrc/model/cbi/qbittorrent/files.lua
@@ -1,28 +1,26 @@
-local fs   = require "nixio.fs"
-local util = require "luci.util"
-local uci  = require "luci.model.uci".cursor()
-
-local config_dir = uci:get("qbittorrent", "main", "RootProfilePath") or "/tmp"
-local files = {}
-files.conf = "/etc/config/qbittorrent"
-files.qbittorrent = config_dir .. "/qBittorrent/config/qBittorrent.conf"
-
-m = SimpleForm("qbittorrent", "%s - %s"%{translate("qBittorrent"), translate("configuration file")},
-	translate("This page is the configuration file content of qBittorrent."))
-m.reset = false
+local m = SimpleForm("qbittorrent", translatef("%s - %s", translate("qBittorrent"), translate("configuration file")),
+    translate("This page is the configuration file content of qBittorrent."))
+m.reset  = false
 m.submit = false
 
-for key, value in pairs(files) do
-	if fs.readfile(value) then
-		s = m:section(SimpleSection, nil, translatef("This is the content of the configuration file under <code>%s</code>:", value))
-		o = s:option(TextValue, key)
-		o.rows = 20
-		o.readonly = true
-		o.cfgvalue = function()
-			local v = fs.readfile(value) or translate("File does not exist.")
-			return util.trim(v) ~= "" and v or translate("Empty file.")
-		end
-	end
+local files = {
+    {name = "conf", path = "/etc/config/qbittorrent"},
+    {name = "qbittorrent", path = (luci.model.uci.cursor():get("qbittorrent", "main", "RootProfilePath") or "/tmp") .. "/qBittorrent/config/qBittorrent.conf"}
+}
+
+for _, file in ipairs(files) do
+    local content = nixio.fs.readfile(file.path)
+    if content then
+        local s = m:section(SimpleSection, nil,
+            translatef("This is the content of the configuration file under <code>%s</code>:", file.path))
+        local o = s:option(TextValue, file.name)
+        o.rows = 20
+        o.readonly = true
+        o.cfgvalue = function()
+            local v = content or translate("File does not exist.")
+            return luci.util.trim(v) ~= "" and v or translate("Empty file.")
+        end
+    end
 end
 
 return m
