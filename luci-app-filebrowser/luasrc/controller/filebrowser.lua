563f17217be99ddb129b25380aadb1dc8d6796a4
hong0980 <hong0980@qq.com>
2023-09-28 19:56:53 +0800

    update@2023-09-28-19-56

--- luci-app-filebrowser/luasrc/controller/filebrowser.lua
+++ luci-app-filebrowser/luasrc/controller/filebrowser.lua
@@ -1,5 +1,6 @@
 local fs   = require "nixio.fs"
 local util = require "luci.util"
+local http = require "luci.http"
 module("luci.controller.filebrowser", package.seeall)
 function index()
     entry({"admin", "system", "filebrowser"}, template("filebrowser"), _("File management"), 60).dependent = true
@@ -8,6 +9,7 @@ function index()
     entry({"admin", "system", "filebrowser_delete"}, call("filebrowser_delete"), nil).leaf = true
     entry({"admin", "system", "filebrowser_rename"}, call("filebrowser_rename"), nil).leaf = true
     entry({"admin", "system", "filebrowser_upload"}, call("filebrowser_upload"), nil).leaf = true
+    entry({"admin", "system", "filebrowser_modify"}, call("filebrowser_modify"), nil).leaf = true
     entry({"admin", "system", "fileassistant", "install"}, call("fileassistant_install"), nil).leaf = true
 end
 
@@ -55,37 +57,25 @@ local MIME_TYPES = {
     avi   = "video/x-msvideo"
 }
 
-function scandir(directory)
-    local t, linkFiles, dirFiles, normalFiles = {}, {}, {}, {}
-    for fileinfo in util.execi("ls -lA '%s'" %directory) do
-        if string.sub(fileinfo, 1, 1) == 'd' then
-            dirFiles[#dirFiles+1] = fileinfo
-        elseif string.sub(fileinfo, 1, 1) == 'l' then
-            linkFiles[#linkFiles+1] = fileinfo
-        else
-            normalFiles[#normalFiles+1] = fileinfo
-        end
-    end
-
-    for _, file in ipairs(dirFiles) do
-        t[#t+1] = file
-    end
-    for _, file in ipairs(normalFiles) do
-        t[#t+1] = file
-    end
-    for _, file in ipairs(linkFiles) do
-        t[#t+1] = file
-    end
-
-    return t
-end
-
 function list_response(path, success)
-    luci.http.prepare_content("application/json")
-    luci.http.write_json({
-        ec = success and 0 or 1,
+    http.prepare_content("application/json")
+    http.write_json({
+        status = success and 0 or 1,
         data = success and scandir(path) or nil
     })
+end
+
+local success = false
+function scandir(dir)
+    local linkFiles, regularFiles = {}, {}
+    for fileinfo in util.execi("ls -Ah --full-time --group-directories-first '%s'" %dir) do
+        if fileinfo:sub(1, 2) == 'lr' then
+            util.append(linkFiles, fileinfo)
+        else
+            util.append(regularFiles, fileinfo)
+        end
+    end
+    return util.combine(regularFiles, linkFiles)
 end
 
 function to_mime(filename)
@@ -98,65 +88,66 @@ function to_mime(filename)
 end
 
 function installIPK(filepath)
-    util.exec('opkg --force-depends install "'..filepath..'"')
-    util.exec('rm -rf /tmp/luci-*')
-    return true
+    success = util.exec('opkg --force-depends install "%s"' %filepath)
+    if success then
+        util.exec('rm -rf "%s"' %filepath)
+    end
+    return success
 end
 
 function filebrowser_list()
-    list_response(luci.http.formvalue("path"), true)
+    list_response(http.formvalue("path"), true)
 end
 
 function filebrowser_open()
-    local path = luci.http.formvalue("path")
-    local filename = luci.http.formvalue("filename")
+    local path = http.formvalue("path")
+    local filename = http.formvalue("filename")
     local mime = to_mime(filename)
-    local file = path .. filename
 
-    luci.http.header('Content-Disposition', 'inline; filename="' .. filename .. '"')
-    luci.http.prepare_content(mime)
-    luci.ltn12.pump.all(luci.ltn12.source.file(io.open(file, "r")), luci.http.write)
+    http.header('Content-Disposition', 'inline; filename="%s"' %filename)
+    http.prepare_content(mime)
+    luci.ltn12.pump.all(luci.ltn12.source.file(io.open(path .. filename, "r")), http.write)
 end
 
 function filebrowser_delete()
-    local path = luci.http.formvalue("path")
-    local isdir = luci.http.formvalue("isdir")
-    path = path:gsub("<>", "/"):gsub(" ", "\\ ")
-    local success = isdir and fs.remove(path)
+    local isdir = http.formvalue("isdir")
+    local path = http.formvalue("path"):gsub("<>", "/"):gsub(" ", "\\ ")
+    success = isdir and util.exec('rm -rf "%s"' %path) or fs.remover(path)
     list_response(fs.dirname(path), success)
 end
 
 function filebrowser_rename()
-    local filepath = luci.http.formvalue("filepath")
-    local newpath = luci.http.formvalue("newpath")
-    local success = fs.mover(filepath, newpath)
+    local filepath = http.formvalue("filepath")
+    local newpath = http.formvalue("newpath")
+    success = fs.move(filepath, newpath)
     list_response(fs.dirname(filepath), success)
 end
 
-function fileassistant_install()
-    local filepath = luci.http.formvalue("filepath")
-    local ext = filepath:match(".+%.(%w+)$")
-    filepath = filepath:gsub("<>", "/"):gsub(" ", "\\ ")
+function filebrowser_modify()
+    local path = luci.http.formvalue("path")
+    local permissions = luci.http.formvalue("permissions")
+    success = permissions and fs.chmod(path, permissions)
+    list_response(fs.dirname(path), success)
+end
 
-    local success = false
-    if ext == "ipk" then
+function fileassistant_install()
+    local filepath = http.formvalue("filepath"):gsub("<>", "/"):gsub(" ", "\\ ")
+    if filepath:match(".+%.(%w+)$") == "ipk" then
         success = installIPK(filepath)
     end
-
     list_response(fs.dirname(filepath), success)
 end
 
 function filebrowser_upload()
-    local filecontent = luci.http.formvalue("upload-file")
-    local filename = luci.http.formvalue("upload-filename")
-    local uploaddir = luci.http.formvalue("upload-dir")
-    local filepath = uploaddir..filename
+    local filecontent = http.formvalue("upload-file")
+    local filename = http.formvalue("upload-filename")
+    local uploaddir = http.formvalue("upload-dir")
 
     local fp
-    luci.http.setfilehandler(
+    http.setfilehandler(
         function(meta, chunk, eof)
             if not fp and meta and meta.name == "upload-file" then
-                fp = io.open(filepath, "w")
+                fp = io.open(uploaddir .. filename, "w")
             end
             if fp and chunk then
                 fp:write(chunk)
