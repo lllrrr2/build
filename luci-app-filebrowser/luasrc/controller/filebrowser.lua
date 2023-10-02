592a2a7bcc66968856787e062176b3c610acbf7e
hong0980 <hong0980@qq.com>
2023-10-02 13:13:06 +0800

    update@2023-10-02-13-13

--- luci-app-filebrowser/luasrc/controller/filebrowser.lua
+++ luci-app-filebrowser/luasrc/controller/filebrowser.lua
@@ -10,10 +10,28 @@ function index()
     entry({"admin", "system", "filebrowser_rename"}, call("filebrowser_rename"), nil).leaf = true
     entry({"admin", "system", "filebrowser_upload"}, call("filebrowser_upload"), nil).leaf = true
     entry({"admin", "system", "filebrowser_modify"}, call("filebrowser_modify"), nil).leaf = true
-    entry({"admin", "system", "fileassistant", "install"}, call("fileassistant_install"), nil).leaf = true
+    entry({"admin", "system", "filebrowser_install"}, call("fileassistant_install"), nil).leaf = true
 end
 
 local MIME_TYPES = {
+    bmp   = "image/bmp",
+    gif   = "image/gif",
+    jpg   = "image/jpeg",
+    jpeg  = "image/jpeg",
+    png   = "image/png",
+    svg   = "image/svg+xml",
+    pdf   = "application/pdf",
+    xml   = "application/xml",
+    xsl   = "application/xml",
+    doc   = "application/msword",
+    ppt   = "application/vnd.ms-powerpoint",
+    xls   = "application/vnd.ms-excel",
+    odt   = "application/vnd.oasis.opendocument.text",
+    odp   = "application/vnd.oasis.opendocument.presentation",
+    zip   = "application/zip",
+    tgz   = "application/x-compressed-tar",
+    deb   = "application/x-deb",
+    iso   = "application/x-cd-image",
     txt   = "text/plain",
     conf  = "text/plain",
     ovpn  = "text/plain",
@@ -28,27 +46,9 @@ local MIME_TYPES = {
     h     = "text/x-chdr",
     o     = "text/x-object",
     ko    = "text/x-object",
-    bmp   = "image/bmp",
-    gif   = "image/gif",
-    png   = "image/png",
-    jpg   = "image/jpeg",
-    jpeg  = "image/jpeg",
-    svg   = "image/svg+xml",
-    zip   = "application/zip",
-    pdf   = "application/pdf",
-    xml   = "application/xml",
-    xsl   = "application/xml",
-    doc   = "application/msword",
-    ppt   = "application/vnd.ms-powerpoint",
-    xls   = "application/vnd.ms-excel",
-    odt   = "application/vnd.oasis.opendocument.text",
-    odp   = "application/vnd.oasis.opendocument.presentation",
     pl    = "application/x-perl",
     sh    = "application/x-shellscript",
     php   = "application/x-php",
-    deb   = "application/x-deb",
-    iso   = "application/x-cd-image",
-    tgz   = "application/x-compressed-tar",
     mp3   = "audio/mpeg",
     ogg   = "audio/x-vorbis+ogg",
     wav   = "audio/x-wav",
@@ -65,6 +65,10 @@ function list_response(path, success)
     })
 end
 
+function replacePathChars(str)
+    return str:gsub("<>", "/"):gsub(" ", "\ ")
+end
+
 local success = false
 function scandir(dir)
     local linkFiles, regularFiles = {}, {}
@@ -78,40 +82,13 @@ function scandir(dir)
     return util.combine(regularFiles, linkFiles)
 end
 
-function to_mime(filename)
-    if type(filename) ~= "string" then
-        return "application/octet-stream"
-    end
-
-    local ext = filename:match("[^%.]+$")
-    return MIME_TYPES[ext and ext:lower()] or "application/octet-stream"
-end
-
-function installIPK(filepath)
-    success = util.exec('opkg --force-depends install "%s"' %filepath)
-    if success then
-        util.exec('rm -rf "%s"' %filepath)
-    end
-    return success
-end
-
 function filebrowser_list()
     list_response(http.formvalue("path"), true)
 end
 
-function filebrowser_open()
-    local path = http.formvalue("path")
-    local filename = http.formvalue("filename")
-    local mime = to_mime(filename)
-
-    http.header('Content-Disposition', 'inline; filename="%s"' %filename)
-    http.prepare_content(mime)
-    luci.ltn12.pump.all(luci.ltn12.source.file(io.open(path .. filename, "r")), http.write)
-end
-
 function filebrowser_delete()
     local isdir = http.formvalue("isdir")
-    local path = http.formvalue("path"):gsub("<>", "/"):gsub(" ", "\\ ")
+    local path = replacePathChars(http.formvalue("path"))
     success = isdir and util.exec('rm -rf "%s"' %path) or fs.remover(path)
     list_response(fs.dirname(path), success)
 end
@@ -124,39 +101,71 @@ function filebrowser_rename()
 end
 
 function filebrowser_modify()
-    local path = luci.http.formvalue("path")
-    local permissions = luci.http.formvalue("permissions")
+    local path = http.formvalue("path")
+    local permissions = http.formvalue("permissions")
     success = permissions and fs.chmod(path, permissions)
     list_response(fs.dirname(path), success)
 end
 
 function fileassistant_install()
-    local filepath = http.formvalue("filepath"):gsub("<>", "/"):gsub(" ", "\\ ")
+    local filepath = replacePathChars(http.formvalue("filepath"))
     if filepath:match(".+%.(%w+)$") == "ipk" then
-        success = installIPK(filepath)
+        success = util.exec('opkg --force-depends install "%s"' %filepath)
     end
-    list_response(fs.dirname(filepath), success)
+    http.prepare_content("application/json")
+    http.write_json({
+        status = success,
+        filepath = filepath
+    })
 end
 
 function filebrowser_upload()
-    local filecontent = http.formvalue("upload-file")
     local filename = http.formvalue("upload-filename")
     local uploaddir = http.formvalue("upload-dir")
-
-    local fp
+    if filename:match(".+%.(%w+)$") == "ipk" then
+        uploaddir = '/tmp/upload/'
+        fs.mkdir(uploaddir)
+    end
+    local fd
     http.setfilehandler(
         function(meta, chunk, eof)
-            if not fp and meta and meta.name == "upload-file" then
-                fp = io.open(uploaddir .. filename, "w")
+            if not fd then
+                if not meta then return end
+                if  meta and chunk then fd = nixio.open(uploaddir .. filename, "w") end
+                if not fd then return end
             end
-            if fp and chunk then
-                fp:write(chunk)
+            if chunk and fd then
+                fd:write(chunk)
             end
-            if fp and eof then
-                fp:close()
+            if eof and fd then
+                fd:close()
+                fd = nil
             end
-      end
+        end
     )
+    http.prepare_content("application/json")
+    http.write_json({
+        status = eof,
+        filename = filename,
+        uploaddir = uploaddir
+    })
+end
 
-    list_response(uploaddir, true)
+function to_mime(filename)
+    if type(filename) ~= "string" then
+        return "application/octet-stream"
+    end
+
+    local ext = filename:match("[^%.]+$")
+    return MIME_TYPES[ext and ext:lower()] or "application/octet-stream"
+end
+
+function filebrowser_open()
+    local path = http.formvalue("path")
+    local filename = http.formvalue("filename")
+    local mime = to_mime(filename)
+
+    http.header('Content-Disposition', 'inline; filename="%s"' %filename)
+    http.prepare_content(mime)
+    luci.ltn12.pump.all(luci.ltn12.source.file(io.open(path .. filename, "r")), http.write)
 end
