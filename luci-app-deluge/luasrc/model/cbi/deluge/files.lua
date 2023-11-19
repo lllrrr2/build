dd015c1bed2fd0dfec6249f56fd81b2208d1b68d
hong09980 <hong09980@qq.com>
2023-11-19 23:35:20 +0800

    update@2023-11-19-23-35

--- luci-app-deluge/luasrc/model/cbi/deluge/files.lua
+++ luci-app-deluge/luasrc/model/cbi/deluge/files.lua
@@ -3,18 +3,18 @@ local util = require "luci.util"
 local uci  = require "luci.model.uci".cursor()
 
 local config_dir = uci:get("deluge", "main", "profile_dir") or "/etc/deluge"
-local files = {}
-files.web  = config_dir .. "/web.conf"
-files.core = config_dir .. "/core.conf"
-files.config = "/etc/config/deluge"
+local conf = {
+	a = "/etc/config/deluge",
+	b = config_dir .. "/core.conf",
+	c = config_dir .. "/web.conf"
+}
 
-m = SimpleForm("deluge", "%s - %s"%{translate("Deluge"), translate("configuration file")},
-	translate("This page is the configuration file content of Deluge."))
+m = SimpleForm("deluge", "Deluge - %s" % {translate("configuration file")}, translate("This page is the configuration file content of Deluge."))
 m.reset = false
 m.submit = false
 
-for key, value in pairs(files) do
-	if fs.readfile(value) then
+for key, value in pairs(conf) do
+	if fs.access(value) then
 		s = m:section(SimpleSection, nil, translatef("This is the content of the configuration file under <code>%s</code>:", value))
 		o = s:option(TextValue, key)
 		o.rows = 20
